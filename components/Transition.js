10e47aeeb7a2dd40070d87bdb7b9e4571018820b
rohithmohanan1 <mohananrohith476@gmail.com>
2023-12-06 16:35:34 +0530

    second day

--- components/Transition.js
+++ components/Transition.js
@@ -1,36 +1,41 @@
-
-import motion from "framer-motion";
+import { motion } from "framer-motion";
 
 const transitionVariants = {
-  initial: {
-    x: '100%',
-    width: '100%',
-  },
-  animate: {
-    x: '0%',
-    width: '0%',
-  },
-  exit: {
-    x: ['0%', '100%'],
-    width: ['0%', '100%'],
-  },
-}
+  initial: { x: '100%', width: '100%' },
+  animate: { x: '0%', width: '0%', transition: { duration: 0.6, ease: 'easeInOut' } },
+  exit: { x: '100%', width: '100%', transition: { duration: 0.6, ease: 'easeInOut' } },
+};
 const Transition = () => {
-  console.log('transitionVariants:', transitionVariants);
+
   return (
     <>
-      <motion.div 
-        className="fixed top-0 bottom-0 right-full h-screen z-30 bg-[#2e2257]" 
-        variants={transitionVariants} 
-        initial='initial'
-        animate='animate' 
-        exit='exit' 
-        transition={{ delay: 0.2, duration: 0.5, ease: 'easeInOut' }}
-      >
-        1
-      </motion.div>
-      <div>2</div>
-      <div>3</div>
+      <motion.div
+        key="1"
+        className="fixed top-0 bottom-0 right-full w-screen h-screen z-30 bg-[#2e2257]"
+        variants={transitionVariants}
+        initial="initial"
+        animate="animate"
+        exit="exit"
+        transition={{ delay: 0.2 }}
+      ></motion.div>
+      <motion.div
+        key="2"
+        className="fixed top-0 bottom-0 right-full w-screen h-screen z-20 bg-[#3b2d71]"
+        variants={transitionVariants}
+        initial="initial"
+        animate="animate"
+        exit="exit"
+        transition={{ delay: 0.4 }}
+      ></motion.div>
+      <motion.div
+        key="3"
+        className="fixed top-0 bottom-0 right-full w-screen h-screen z-10 bg-[#4b3792]"
+        variants={transitionVariants}
+        initial="initial"
+        animate="animate"
+        exit="exit"
+        transition={{ delay: 0.6 }}
+      ></motion.div>
     </>
   );
 };
